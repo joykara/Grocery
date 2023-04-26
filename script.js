e705bfab362e25a1085cf04697a74e143e20f439
joykara <joykarani1@gmail.com>
2023-04-26 23:26:41 +0300

    alert

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const alert = document.querySelector('.alert')
-const form = document.querySelector('#grocery-form')
-const grocery = document.getElementById('grocery')
-const submitBtn = document.querySelector('.submit-btn')
-const container = document.querySelector('.grocery-container')
-const list = document.querySelector('.grocery-list')
-const item = document.querySelector('.grocery-item')
-const clearBtn = document.querySelector('.clear-btn')
+const alert = document.querySelector('.alert');
+const form = document.querySelector('#grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const item = document.querySelector('.grocery-item');
+const clearBtn = document.querySelector('.clear-btn');
 
 let editElement;
 let editFlag = false;
 let editID = "";
 
 //submit form
-form.addEventListener('submit', addItem)
+form.addEventListener('submit', addItem);
 
 function addItem(e) {
     e.preventDefault();
-    const value = grocery.value
-    const id = grocery.id
-    console.log(value)
-    if (value !== '' && editFlag == false) {
-        console.log("Add item to list")
+    const value = grocery.value;
+    const id = grocery.id;
+    console.log(value);
+    if (value && !editFlag) {
+        console.log("Add item to list");
     }
-    else if (value === '' && editFlag == true) {
-        console.log("Editing")
+    else if (value && editFlag) {
+        console.log("Editing");
     }
     else {
-        console.log("Empty value")
+        alert.textContent = "Empty value";
+        alert.classList.add('alert-danger');
     }
 }
